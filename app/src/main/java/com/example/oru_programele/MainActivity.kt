076adbe35aada83faece1643ecf56e7e6753aff7
lukas <lukaskrun@gmail.com>
2023-02-13 16:06:06 +0200

    Added empty kotlin project

---
+++ app/src/main/java/com/example/oru_programele/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.oru_programele
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
